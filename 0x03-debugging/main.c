df4f4dba7235140eaeeed8138a5216b9f9b5ef7d
Noswazi <Noswazi18@gmail.com>
2023-02-23 06:49:41 -0800

    Multiple mains

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* main - Function that prints if integer is positive_or_negative
+* Return: 0
+*/
+
+int main(void);
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+	return (0);
+}
