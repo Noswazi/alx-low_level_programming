f1d0eebdec26a4cafe829e626905da936501920b
Noswazi <Noswazi18@gmail.com>
2023-03-18 12:01:57 -0700

    0. Trust no one

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,9 +1,9 @@
 #include "main.h"
 #include <stdlib.h>
-
+#include <stdio.h>
 /**
  * malloc_checked - allocates memory
- * @b - number of bytes
+ * @b: number of bytes
  *
  * Return: pointer to the allocated memory
  * status value equal to 98
