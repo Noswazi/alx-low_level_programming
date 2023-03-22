2814153a83fbd4f7145f3d4e57780c6fa5442254
Noswazi <Noswazi18@gmail.com>
2023-03-22 06:44:07 -0700

    2. To hell with circumstances; I create opportunities

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,16 +1,16 @@
 #include "function_pointers.h"
 /**
  * int_index - funtion that searches for integer
- * array: array
+ * @array: array
  * @size: size of array
  * @cmp: pointer to function
- * Return: Always 0 (success)
+ * Return: 0
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size < 1 || array == NULL || cmp == NULL)
+	if (array == NULL || size <= 0 || cmp == NULL)
 		return (-1);
 
 	for (i = 0; i < size; i++)
