cf1b4bc722dedd0a10d8591937910ce5493d1c44
Noswazi <Noswazi18@gmail.com>
2023-04-10 02:00:33 -0700

    11

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * set_bit - sets value of bit to 1
+ * at a given index
+ * @n: number to be updated
+ * @index: index of the bit
+ *
+ * Return: return 1 if (SUCCESS) or -1 if error
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > sizeof(n) * 8)
+		return (-1);
+	{
+		*n |= 11u << index;
+	}
+
+	return (1);
+}
